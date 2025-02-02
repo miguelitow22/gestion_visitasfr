ad1845abb5bbabd5ee4aada9a457b3c8a2acea48
Miguel Lopez <miguelopsal@gmail.com>
2025-02-01 21:26:56 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import Consultas from './Consultas';
-import Programar from './Programar';
-import Reportar from './Reportar';
-import Navbar from './Navbar';
-import Footer from './Footer';
-import './styles.css';
+import Home from './pages/Home';
+import Consultas from './pages/Consultas';
+import Programar from './pages/Programar';
+import Reportar from './pages/Reportar';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+
 
 function App() {
   return (
     <Router>
+      <Header />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
