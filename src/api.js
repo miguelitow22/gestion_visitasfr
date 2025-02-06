f9e235135fe28aa8ab3dd49a5b876785a8783ef5
Miguel Lopez <miguelopsal@gmail.com>
2025-02-06 16:16:24 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || "https://gestionvisitas-production.up.railway.app"
+const API_BASE_URL = import.meta.env.VITE_API_URL 
 
 
 // ✅ Expresión regular para validar emails
