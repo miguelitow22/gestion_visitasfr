561dc681c8e47a576318fb80b0a0f89aced4434e
Miguel Lopez <miguelopsal@gmail.com>
2025-02-06 19:03:08 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL,
+        target: process.env.VITE_API_URL, // ← Usa `process.env` en lugar de `import.meta.env`
         changeOrigin: true,
         secure: false,
       },
